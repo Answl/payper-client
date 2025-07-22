8978297a51ec70ff7b2c4e053d35d2e83756c67c
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-07-22 14:16:01 +0900

    [FEATURE #10]: 테스트 환경 설정 (#12)
    * feature: Axios 설치
    
    * feature: 추가 라이브러리 설치
    
    * feature: 테스트 설정 수정

--- src/components/__tests__/example.test.ts
+++ src/components/__tests__/example.test.ts
@@ -3,3 +3,13 @@ import { expect, it } from "vitest";
 it("1 + 1 should be 2", () => {
   expect(1 + 1).toBe(2);
 });
+
+it("responds with the user", async () => {
+  const response = await fetch("https://api.example.com/user");
+
+  await expect(response.json()).resolves.toEqual({
+    id: "abc-123",
+    firstName: "John",
+    lastName: "Maverick",
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from "node:url";
+import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
+      environment: "jsdom",
+      exclude: [...configDefaults.exclude, "e2e/**"],
+      root: fileURLToPath(new URL("./", import.meta.url)),
+      globals: true, // Vitest API를 글로벌 스코프에서 사용 가능 (describe, it 등)
+      setupFiles: ["./vitest.setup.ts"], // MSW 초기 설정을 위한 파일
     },
-  }),
-)
+  })
+);
