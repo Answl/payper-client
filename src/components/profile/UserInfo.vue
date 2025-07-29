73970da30d7845700047d0285f528a1fdcf99271
Gyehyun Park <167384362+gyehyun-bak@users.noreply.github.com>
2025-07-29 17:11:15 +0900

    [FEATURE #109]: ProfilePage.vue 구현 (#110)
    feature: ProfilePage 수정

---
+++ src/components/profile/UserInfo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { User } from "@/types/User";
+
+const { user } = defineProps<{
+  user: User;
+}>();
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">{{ user.name }}</h1>
+  </div>
+</template>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,68 +1,47 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import { CommonButton } from "@/components/ui/button";
-import { ChevronRight, Pencil, LogOut, UserX, ArrowLeft } from "lucide-vue-next";
+import CommonHeader from "@/components/CommonHeader.vue";
+import { useMeQuery } from "@/composables/user.query";
+import UserInfo from "@/components/profile/UserInfo.vue";
+import MenuButton from "@/components/MenuButton.vue";
+import { Pencil, UserRoundX } from "lucide-vue-next";
+import BottomNavigation from "@/components/common/BottomNavigation.vue";
 
 const router = useRouter();
+const { data, isSuccess } = useMeQuery();
 
-const back = () => router.back();
-// 프로필 수정 기능 추가 예정
-const editInfo = () => router.push("/profile/edit");
-const logout = () => router.push("/logout");
-// 계정 삭제 기능 추가 예정
-const deleteAccount = () => router.push("/delete-account");
+const editProfile = () => {
+  router.push("/profile/edit");
+};
+
+const withdrawal = () => {
+  console.log("회원탈퇴");
+};
+
+if (!isSuccess) {
+  router.push("/landing");
+}
 </script>
 <template>
-  <div class="min-h-screen bg-gray-100 px-4 py-8">
-    <div class="w-full max-w-md mx-auto bg-white p-6 rounded-2xl shadow-md">
-      <div class="flex items-center justify-between mb-6">
-        <ArrowLeft class="w-5 h-5 text-gray-600 cursor-pointer" @click="back" />
-        <h2 class="text-lg font-semibold text-center flex-1 -ml-5">내 정보</h2>
+  <div class="flex flex-col size-full gap-5">
+    <CommonHeader title="프로필" />
+    <div class="flex flex-col gap-10 size-full px-[30px]">
+      <UserInfo v-if="data" :user="data" />
+      <div class="flex flex-col size-full gap-5">
+        <MenuButton
+          data-testid="edit-profile-button"
+          label="내 정보 수정하기"
+          :icon="Pencil"
+          :onClick="editProfile"
+        />
+        <MenuButton
+          data-testid="withdrawal-button"
+          label="회원탈퇴"
+          :icon="UserRoundX"
+          :onClick="withdrawal"
+        />
       </div>
-
-      <div class="mb-6 px-2">
-        <p class="text-base font-bold text-gray-800">김종이</p>
-        <p class="text-sm text-gray-500">000-0000-0000</p>
-      </div>
-
-      <CommonButton
-        as="button"
-        variant="ghost"
-        class="w-full flex justify-between items-center border px-4 py-4 rounded-md shadow-sm mb-6"
-        @click="editInfo"
-      >
-        <div class="flex items-center space-x-2 text-gray-800">
-          <Pencil class="w-5 h-5" />
-          <span>내 정보 수정하기</span>
-        </div>
-        <ChevronRight class="w-4 h-4 text-gray-400" />
-      </CommonButton>
-
-      <CommonButton
-        as="button"
-        variant="ghost"
-        class="w-full flex justify-between items-center border px-4 py-4 rounded-md shadow-sm mb-6"
-        @click="logout"
-      >
-        <div class="flex items-center space-x-2 text-gray-800">
-          <LogOut class="w-5 h-5" />
-          <span>로그아웃</span>
-        </div>
-        <ChevronRight class="w-4 h-4 text-gray-400" />
-      </CommonButton>
-
-      <CommonButton
-        as="button"
-        variant="ghost"
-        class="w-full flex justify-between items-center border px-4 py-4 rounded-md shadow-sm"
-        @click="deleteAccount"
-      >
-        <div class="flex items-center space-x-2 text-gray-800">
-          <UserX class="w-5 h-5" />
-          <span>회원탈퇴</span>
-        </div>
-        <ChevronRight class="w-4 h-4 text-gray-400" />
-      </CommonButton>
     </div>
+    <BottomNavigation selected="profile" />
   </div>
 </template>
