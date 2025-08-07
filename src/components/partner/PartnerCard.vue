76bd4bafec312e838f35c5a02c6e82579df47a1c
jdh9864 <114468795+jdh9864@users.noreply.github.com>
2025-08-07 13:37:25 +0900

    [REFACTOR #242]: 가맹점 상세페이지 수정 (#243)
    refactor: 가맹점 상세 페이지 수정

--- src/components/partner/PartnerCard.vue
+++ src/components/partner/PartnerCard.vue
@@ -16,22 +16,16 @@ const onClick = () => {
 <template>
   <div @click="onClick" class="flex gap-5 w-full hover:cursor-pointer">
     <img :src="card.imageUrl" class="w-24 h-full object-cover" />
-    <div class="flex flex-col gap-2 w-full">
+    <div class="flex flex-col gap-2 w-full h-full justify-between">
       <div class="flex flex-col">
         <p class="text-stone-500 text-sm">{{ card.company.name }}</p>
         <p class="text-stone-900">{{ card.name }}</p>
       </div>
-      <p class="text-stone-500 text-sm">
-        ※ 이용금액 건당 1만원 이상 시<br />
-        ※ 상품권, 선불카드 구입(충전) 제외
-      </p>
       <div class="flex flex-col gap-2 items-end">
-        <!-- <p class="text-stone-900 text-sm">전월실적 {{ card.grades[0].start / 10000 }}만원 이상</p> -->
-        <div class="flex gap-2 items-center">
-          <p class="text-stone-500 text-sm">예상혜택</p>
-          <p class="text-primary font-bold">
-            <!-- {{ card.grades?.[0].totalDiscount?.toLocaleString?.() || "0" }}원 할인 -->
-            <!-- {{ card.grades[0].totalDiscount.toLocaleString() }}원 할인 -->
+        <div v-if="card.benefits && card.benefits.length > 0" class="flex flex-col gap-1 items-end">
+          <p class="text-stone-500 text-sm">{{ card.preMonthSpending ? card.preMonthSpending : "전월 실적 정보 없음" }}</p>
+          <p  class="text-primary font-bold">
+            {{ card.benefits[0].summary }}
           </p>
         </div>
       </div>
