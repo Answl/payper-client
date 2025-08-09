cf53f14e5e167e37a10466b2ad3c0254e1bd13ec
Kim Eun Ji <118355919+Answl@users.noreply.github.com>
2025-08-09 09:40:30 +0900

    [FEATURE #277]: pwa 플러그인 설치 및 서비스워커 등록 (#278)
    * feature: pwa 플러그인 설치
    
    * refactor: vite.config.ts 수정
    
    * feature: 서비스 워커 등록
    
    * test: 임시 icon 등록

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import router from "./router";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import { useKakao } from "vue3-kakao-maps";
 
+import { registerSW } from 'virtual:pwa-register';
+
 // Browser에서 MSW 활성화
 async function enableMocking() {
   // 개발 환경에서만 동작
@@ -19,6 +21,18 @@ async function enableMocking() {
 }
 
 useKakao(import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY);
+
+// 서비스 워커 등록
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("새 버전이 있습니다. 새로고침할까요?")) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    console.log("앱이 오프라인에서도 준비되었습니다!");
+  },
+});
 
 enableMocking().then(() => {
   const app = createApp(App);
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,43 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(), 
+    vueDevTools(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "payper",
+        short_name: "payper",
+        description: "내가 가진 카드로, 지금 내 주변 매장에서 받을 수 있는 혜택을 확인하는 서비스",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        clientsClaim: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
